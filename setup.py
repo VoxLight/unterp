ebf4fa53d514acb4297832d8807955f42b3d6c99
VoxLight <tkkt392@gmail.com>
2024-06-09 12:09:39 -0400

    Updated Setup Metadata

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='unterp',
-    description='Minimalistic Python interpreter',
-
-    version='0.1.0',
+    version='1.0.0',
+    description='Minimalistic Python Interpreter with a GUI',
     author='VoxLight',
-
+    author_email='tkkt392@gmail.com',
+    url='https://github.com/VoxLight/unterp',
     packages=find_packages(),
-
+    include_package_data=True,
+    package_data={
+        '': ['icons/*.ico', 'icons/*.png'],
+    },
     install_requires=[
-        'pygments',
+        'Pillow',
+        'Pygments',
     ],
-
     entry_points={
         'console_scripts': [
             'unterp=unterp.__main__:main',
         ],
     },
-
 )
